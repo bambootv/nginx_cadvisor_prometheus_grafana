e38a1f8fc8b3146854ec5a3a8711aa80f1ffdddc
ducpt555 <ducpt@ecomobile.vn>
2026-03-16 08:27:44 +0700

    Apply victoria

--- hybrid/terraform/modules/vps/main.tf
+++ hybrid/terraform/modules/vps/main.tf
@@ -32,6 +32,23 @@ resource "grafana_data_source" "prometheus" {
   }
 }
 
+# 1b. Create VictoriaMetrics Datasource via PDC (Prometheus-compatible)
+resource "grafana_data_source" "victoriametrics" {
+  name = "VictoriaMetrics-${var.vps_name}"
+  type = "prometheus"
+  url  = "http://victoria-metrics:8428" # Local URL inside the VPS network
+
+  # PDC Configuration
+  json_data_encoded = jsonencode({
+    enableSecureSocksProxy   = true
+    secureSocksProxyUsername = var.pdc_cluster_id
+  })
+
+  http_headers = {
+    "X-Pdc-Connection" = var.pdc_cluster_id
+  }
+}
+
 # 2. Create Loki Datasource via PDC
 resource "grafana_data_source" "loki" {
   name = "Loki-${var.vps_name}"
@@ -49,14 +66,40 @@ resource "grafana_data_source" "loki" {
   }
 }
 
+# 2b. Create VictoriaLogs Datasource via PDC (requires Grafana plugin)
+# Plugin: https://grafana.com/grafana/plugins/victoriametrics-logs-datasource/
+resource "grafana_data_source" "victorialogs" {
+  name = "VictoriaLogs-${var.vps_name}"
+  type = "victoriametrics-logs-datasource"
+  url  = "http://victoria-logs:9428" # Local URL inside the VPS network
+
+  # PDC Configuration
+  json_data_encoded = jsonencode({
+    enableSecureSocksProxy   = true
+    secureSocksProxyUsername = var.pdc_cluster_id
+  })
+
+  http_headers = {
+    "X-Pdc-Connection" = var.pdc_cluster_id
+  }
+}
+
 # 3. Create Folder for this VPS
 resource "grafana_folder" "vps_folder" {
   title = "VPS Monitoring (${var.vps_name})"
 }
 
+locals {
+  dashboards_dir = "${path.module}/../../../../common/dashboards"
+  dashboards = {
+    for f in fileset(local.dashboards_dir, "*.json") :
+    f => jsondecode(file("${local.dashboards_dir}/${f}"))
+  }
+}
+
 # 4. Deploy Dashboards to this Folder
 resource "grafana_dashboard" "dashboards" {
-  for_each = fileset("${path.module}/../../../../common/dashboards", "*.json")
+  for_each = local.dashboards
 
   folder = grafana_folder.vps_folder.id
 
@@ -66,14 +109,22 @@ resource "grafana_dashboard" "dashboards" {
     jsondecode(
       replace(
         replace(
-          file("${path.module}/../../../../common/dashboards/${each.key}"),
-          "\"uid\": \"loki\"", "\"uid\": \"${grafana_data_source.loki.uid}\""
+          replace(
+            replace(
+              file("${local.dashboards_dir}/${each.key}"),
+              "\"uid\": \"loki\"", "\"uid\": \"${grafana_data_source.loki.uid}\""
+            ),
+            "\"uid\": \"victorialogs\"", "\"uid\": \"${grafana_data_source.victorialogs.uid}\""
+          ),
+          "\"uid\": \"prometheus\"", "\"uid\": \"${grafana_data_source.prometheus.uid}\""
         ),
-        "\"uid\": \"prometheus\"", "\"uid\": \"${grafana_data_source.prometheus.uid}\""
+        "\"uid\": \"victoriametrics\"", "\"uid\": \"${grafana_data_source.victoriametrics.uid}\""
       )
     ),
     {
-      uid   = "${jsondecode(file("${path.module}/../../../../common/dashboards/${each.key}")).uid}-${var.vps_name}"
+      # Grafana dashboard UID max length is 40 chars.
+      # Keep the readable UID when it fits; otherwise fall back to a stable short hash.
+      uid = length("${each.value.uid}-${var.vps_name}") <= 40 ? "${each.value.uid}-${var.vps_name}" : substr(md5("${each.value.uid}-${var.vps_name}"), 0, 32)
     }
   ))
 }
